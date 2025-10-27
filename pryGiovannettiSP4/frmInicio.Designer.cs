c988f8122a57f85aab215a8ec0ab3b9387437543
juan ignacio giovannetti <giovannettijuan123@gmail.com>
2025-10-27 18:29:10 -0300

    cambios

--- pryGiovannettiSP4/frmInicio.Designer.cs
+++ pryGiovannettiSP4/frmInicio.Designer.cs
@@ -41,8 +41,9 @@
             btnMozo = new Button();
             btnImporteTotal = new Button();
             btnCerrar = new Button();
-            lblTotal = new Label();
             lblMozoDelDia = new Label();
+            lblMozoGanador = new Label();
+            lblTotal = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
             SuspendLayout();
             // 
@@ -52,9 +53,9 @@
             lblDatosDeVenta.BackColor = Color.White;
             lblDatosDeVenta.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblDatosDeVenta.ForeColor = Color.Black;
-            lblDatosDeVenta.Location = new Point(22, 21);
+            lblDatosDeVenta.Location = new Point(25, 28);
             lblDatosDeVenta.Name = "lblDatosDeVenta";
-            lblDatosDeVenta.Size = new Size(178, 25);
+            lblDatosDeVenta.Size = new Size(227, 32);
             lblDatosDeVenta.TabIndex = 0;
             lblDatosDeVenta.Text = "Ingrese Sus Ventas";
             // 
@@ -62,35 +63,47 @@
             // 
             dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvDatos.Columns.AddRange(new DataGridViewColumn[] { colMozos, colComidas, ColBebidaAlc, ColBebidasSinAlc, ColPostres });
-            dgvDatos.Location = new Point(22, 70);
+            dgvDatos.Location = new Point(25, 93);
+            dgvDatos.Margin = new Padding(3, 4, 3, 4);
             dgvDatos.Name = "dgvDatos";
-            dgvDatos.Size = new Size(544, 150);
+            dgvDatos.RowHeadersWidth = 51;
+            dgvDatos.Size = new Size(622, 200);
             dgvDatos.TabIndex = 1;
             // 
             // colMozos
             // 
             colMozos.HeaderText = "Mozos";
+            colMozos.MinimumWidth = 6;
             colMozos.Name = "colMozos";
+            colMozos.Width = 125;
             // 
             // colComidas
             // 
             colComidas.HeaderText = "Comidas";
+            colComidas.MinimumWidth = 6;
             colComidas.Name = "colComidas";
+            colComidas.Width = 125;
             // 
             // ColBebidaAlc
             // 
             ColBebidaAlc.HeaderText = "Bebidas Con Alcohol";
+            ColBebidaAlc.MinimumWidth = 6;
             ColBebidaAlc.Name = "ColBebidaAlc";
+            ColBebidaAlc.Width = 125;
             // 
             // ColBebidasSinAlc
             // 
             ColBebidasSinAlc.HeaderText = "Bebidas Sin Alcohol";
+            ColBebidasSinAlc.MinimumWidth = 6;
             ColBebidasSinAlc.Name = "ColBebidasSinAlc";
+            ColBebidasSinAlc.Width = 125;
             // 
             // ColPostres
             // 
             ColPostres.HeaderText = "Postres";
+            ColPostres.MinimumWidth = 6;
             ColPostres.Name = "ColPostres";
+            ColPostres.Width = 125;
             // 
             // lblTotalGeneral
             // 
@@ -98,9 +111,9 @@
             lblTotalGeneral.BackColor = Color.White;
             lblTotalGeneral.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTotalGeneral.ForeColor = Color.Black;
-            lblTotalGeneral.Location = new Point(22, 271);
+            lblTotalGeneral.Location = new Point(307, 499);
             lblTotalGeneral.Name = "lblTotalGeneral";
-            lblTotalGeneral.Size = new Size(115, 21);
+            lblTotalGeneral.Size = new Size(143, 28);
             lblTotalGeneral.TabIndex = 2;
             lblTotalGeneral.Text = "Total General:";
             // 
@@ -109,21 +122,24 @@
             btnValidarDatos.BackColor = Color.BurlyWood;
             btnValidarDatos.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnValidarDatos.ForeColor = Color.White;
-            btnValidarDatos.Location = new Point(161, 314);
+            btnValidarDatos.Location = new Point(119, 313);
+            btnValidarDatos.Margin = new Padding(3, 4, 3, 4);
             btnValidarDatos.Name = "btnValidarDatos";
-            btnValidarDatos.Size = new Size(101, 37);
+            btnValidarDatos.Size = new Size(133, 49);
             btnValidarDatos.TabIndex = 3;
             btnValidarDatos.Text = "Validar Datos";
             btnValidarDatos.UseVisualStyleBackColor = false;
+            btnValidarDatos.Click += btnValidarDatos_Click;
             // 
             // btnMozo
             // 
             btnMozo.BackColor = Color.BurlyWood;
             btnMozo.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnMozo.ForeColor = Color.White;
-            btnMozo.Location = new Point(310, 314);
+            btnMozo.Location = new Point(25, 415);
+            btnMozo.Margin = new Padding(3, 4, 3, 4);
             btnMozo.Name = "btnMozo";
-            btnMozo.Size = new Size(101, 37);
+            btnMozo.Size = new Size(133, 49);
             btnMozo.TabIndex = 4;
             btnMozo.Text = "Mozo Del dia ";
             btnMozo.UseVisualStyleBackColor = false;
@@ -132,9 +148,10 @@
             // 
             btnImporteTotal.BackColor = Color.BurlyWood;
             btnImporteTotal.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnImporteTotal.Location = new Point(465, 314);
+            btnImporteTotal.Location = new Point(25, 491);
+            btnImporteTotal.Margin = new Padding(3, 4, 3, 4);
             btnImporteTotal.Name = "btnImporteTotal";
-            btnImporteTotal.Size = new Size(101, 37);
+            btnImporteTotal.Size = new Size(133, 49);
             btnImporteTotal.TabIndex = 5;
             btnImporteTotal.Text = "Totales";
             btnImporteTotal.UseVisualStyleBackColor = false;
@@ -144,45 +161,56 @@
             btnCerrar.BackColor = Color.BurlyWood;
             btnCerrar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnCerrar.ForeColor = Color.White;
-            btnCerrar.Location = new Point(22, 314);
+            btnCerrar.Location = new Point(532, 560);
+            btnCerrar.Margin = new Padding(3, 4, 3, 4);
             btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(101, 37);
+            btnCerrar.Size = new Size(115, 49);
             btnCerrar.TabIndex = 6;
             btnCerrar.Text = "Cerrar";
             btnCerrar.UseVisualStyleBackColor = false;
-            // 
-            // lblTotal
-            // 
-            lblTotal.AutoSize = true;
-            lblTotal.BackColor = Color.White;
-            lblTotal.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblTotal.ForeColor = Color.Black;
-            lblTotal.Location = new Point(140, 275);
-            lblTotal.Name = "lblTotal";
-            lblTotal.Size = new Size(60, 17);
-            lblTotal.TabIndex = 7;
-            lblTotal.Text = ". . . . . . .";
-            lblTotal.Click += lblTotal_Click;
             // 
             // lblMozoDelDia
             // 
             lblMozoDelDia.AutoSize = true;
             lblMozoDelDia.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblMozoDelDia.ForeColor = Color.Black;
-            lblMozoDelDia.Location = new Point(20, 241);
+            lblMozoDelDia.Location = new Point(304, 436);
             lblMozoDelDia.Name = "lblMozoDelDia";
-            lblMozoDelDia.Size = new Size(117, 21);
+            lblMozoDelDia.Size = new Size(146, 28);
             lblMozoDelDia.TabIndex = 8;
             lblMozoDelDia.Text = "Mozo Del Dia:";
             // 
+            // lblMozoGanador
+            // 
+            lblMozoGanador.AutoSize = true;
+            lblMozoGanador.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblMozoGanador.ForeColor = Color.Black;
+            lblMozoGanador.Location = new Point(473, 441);
+            lblMozoGanador.Name = "lblMozoGanador";
+            lblMozoGanador.Size = new Size(55, 23);
+            lblMozoGanador.TabIndex = 9;
+            lblMozoGanador.Text = ". . . . .";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotal.ForeColor = Color.Black;
+            lblTotal.Location = new Point(473, 504);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(55, 23);
+            lblTotal.TabIndex = 10;
+            lblTotal.Text = ". . . . .";
+            // 
             // frmInicio
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(618, 413);
-            Controls.Add(lblMozoDelDia);
+            ClientSize = new Size(664, 619);
             Controls.Add(lblTotal);
+            Controls.Add(lblMozoGanador);
+            Controls.Add(lblMozoDelDia);
             Controls.Add(btnCerrar);
             Controls.Add(btnImporteTotal);
             Controls.Add(btnMozo);
@@ -192,8 +220,10 @@
             Controls.Add(lblDatosDeVenta);
             ForeColor = Color.White;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             Name = "frmInicio";
             Text = "Ventas La Milanga";
+            Load += frmInicio_Load;
             ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -213,7 +243,8 @@
         private Button btnMozo;
         private Button btnImporteTotal;
         private Button btnCerrar;
-        private Label lblTotal;
         private Label lblMozoDelDia;
+        private Label lblMozoGanador;
+        private Label lblTotal;
     }
 }
--- pryGiovannettiSP4/frmInicio.cs
+++ pryGiovannettiSP4/frmInicio.cs
@@ -7,9 +7,50 @@ namespace pryGiovannettiSP4
             InitializeComponent();
         }
 
-        private void lblTotal_Click(object sender, EventArgs e)
-        {
+        string comida = "";
+        string bebida = "";
+        string bebidaAlc = "";
+        string postres = "";
+        int fila = 0;
+        int col = 0;
+        int total = 0;
 
+
+        private void frmInicio_Load(object sender, EventArgs e)
+        {
+            dgvDatos.Rows.Add("Julio", 0, 0, 0, 0);
+            dgvDatos.Rows.Add("Esteban", 0, 0, 0, 0);
+            dgvDatos.Rows.Add("Javier", 0, 0, 0, 0);
+            dgvDatos.Rows.Add("Gonzalo", 0, 0, 0, 0);
+            dgvDatos.Rows.Add("Alberto", 0, 0, 0, 0);
+        }
+
+        private void btnValidarDatos_Click(object sender, EventArgs e)
+        {
+            fila = 0;
+            while (fila < 4)
+            {
+                col = 1;
+                while (col < 5)
+                {
+                    if (Convert.ToDecimal(dgvDatos.Rows[fila].Cells[col].Value) < 0)
+                    {
+                        MessageBox.Show("Los valores ingresados son incorrectos");
+                        dgvDatos.Rows[fila].Cells[col].Value = 0;
+                    }
+                    col++;
+                }
+                fila++;
+
+                //Mensaje de datos validados correctamente
+                if (fila == 5 && col == 4)
+                {
+                    MessageBox.Show("Los datos ingresados son correctos");
+                    btnMozo.Enabled = true;
+                    btnImporteTotal.Enabled = true;
+
+                }
+            }
         }
     }
 }
